43829cbc9246a6c0125ac6005710d84d3a650cba
SAntiGB05 <santigbttobi@gmail.com>
2024-10-31 22:00:47 -0500

    se puso el footer se mejoraron las cards y se agregaron iconos

--- js/app.js
+++ js/app.js
@@ -4,14 +4,16 @@ function createProductDivs() {
     const container = document.getElementById('container');
     products.forEach(product => {
         const newDiv = document.createElement('div');
-        newDiv.className = 'info-card';
+        newDiv.className = 'info-card style="width: 18rem;';
 
         newDiv.innerHTML= `
-            <img src="${product.src}" alt="">
-            <h4>Nombre: ${product.name}</h4>
-            <p>Descripción: ${product.description}</p>
-            <p>Precio: $${product.price.toFixed(2)}</p>
-            <button> Añadir </button>
+                <img src="${product.src}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <p class="card-text">$${product.price.toFixed(2)}</p>
+                    <a href="#" class="btn btn-primary">Añadir</a>
+                </div>
         `;
 
         container.appendChild(newDiv);
